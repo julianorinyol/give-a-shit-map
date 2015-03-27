4b87bc4d1909d3a381b156d78968cb04f95e48d5
JulianOrinyol <julianorinyol@gmail.com>
2015-03-27 12:17:38 -0700

    Set up basic retrieval from db, converting AR to JSON and requesting with ajax. actions added, and routes.

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,21 @@
 # Homepage (Root path)
+helpers do
+  @states = State.all
+end
+
+
 
 get '/' do
   erb :index
 end
+
+get '/states' do
+  @states = State.all
+  @states.to_json
+end
+
+get '/states/:state_code' do
+  @state = State.find_by(state_code: params[:state_code])
+  @state.to_json
+end
+
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,67 @@
-$(document).ready(function() {
+$(function() {
+    var states = []
+    $.getJSON('/states')
+    .success(function(data){
+      states = data;
+      console.log('states', states);
+    })
+    console.log('state 2', states);
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var map = kartograph.map('#map');
+    map.loadMap('usa.svg', function() {
+        console.log("start of loadmap callback")
+        map.addLayer('layer0', {
+            styles: {
+                stroke: '#aaa',
+                fill: '#f6f4f2'
+            },
+            mouseenter: function(data, path) {
+                path.attr('fill', Math.random() < 0.5 ? '#c04' : '#04c');
+            },
+            click: function(svgState, path) {
+                // console.log(d)
+                // console.log(d["key"])
+                // console.log(d["label"])
+                // $("#superinfo").html("<p>Hello " + svgState.label + "</p>")
+
+                var stateCode = svgState.key
+
+                var updateSuperinfo = function(state){
+                  $("#superinfo").html("<p> Hello from " + state.capital + "</p>")
+                }
+                $.getJSON('/states/' + svgState.key)
+                .success(updateSuperinfo)
+                .error(function(status, error){
+                  console.log("something went wrong", status, error);
+                })
+
+                //var currentState = <% State.last.name %>
+                console.log(svgState)
+                // $("#testytest").append("<p>"<% State.find(1).name %>"</p>")
+            },
+            mouseleave: function(d, path) {
+                path.animate({ fill: '#f6f4f2' }, 200);
+            }
+        
+        });
+
+        var points_of_interest = [
+            { name: 'Juneau, AK', lat: 58.3, lon: -134.416667 },
+            { name: 'Honolulu, HI', lat: 21.3, lon: -157.816667 },
+            { name: 'San Francisco, CA', lat: 37.783333, lon: -122.416667 },
+        ];
+
+        map.addSymbols({
+            type: kartograph.LabeledBubble,
+            data: points_of_interest,
+            location: function(d) { return [d.lon, d.lat] },
+            title: function(d) { return d.name; },
+            radius: 3,
+            center: false,
+            attrs: { fill: 'black' },
+            labelattrs: { 'font-size': 11 },
+            buffer: true
+        });
+
+    }, { padding: 30 });
 });
