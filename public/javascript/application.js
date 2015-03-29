99ed132b103df80780c89f58b1e5d4cdf149e906
JulianOrinyol <julianorinyol@gmail.com>
2015-03-28 19:24:40 -0700

    updated text in erb and application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -7,6 +7,8 @@ $(function() {
     })
     console.log('state 2', states);
 
+    var mode = "education"
+
     var map = kartograph.map('#map');
     map.loadMap('usa.svg', function() {
         console.log("start of loadmap callback")
@@ -34,12 +36,25 @@ $(function() {
 
                 var stateCode = svgState.key
 
+
+
+
                 var updateSuperInfo = function(state, x, y){
                     console.log(state);
+
+                    var educationContainer1 = "<p> Libraries per capita" + ".12"+ "</p>" + "<p>Pupil/Teacher ratio: " + "14.3" + "</p>"
+
+
+                    var educationContainer2 = "<p>"+" data here"+ "</p>"
+                    
+                    var educationContainer3 =  "<p>"+ "Graduated high school: 17%" + "</p>" + "<p>Bachelors Degree: 25% </p>" + "<p>"+ " Advanced degree: 17%</p>" + "<p>*Percentage of adult population over 25 years old</p>"
+
                     $("#superinfo").html("<h1> Hello from " + state["name"] + "</h1>")
-                    $("#container1").html("<p> In a randomized telephone survey of women 18 years and older " + state.poor_health + "% reported having \"fair or poor\" health. </p>" + "<p>" + "An adult who has a BMI between 25 and 29.9 is considered overweight. An adult who has a BMI of 30 or higher is considered obese. " + state.obesity  + "% of women in " + state.name +" are overweight or obese" + "</p>")
-                    $("#container2").html("<p>Out of every 1000 teenage girls between 15 and 19 years, " + state.teen_birth + " have had at least one child.</p>") 
-                    $("#container3").html("<p>" + state.doctor + "% of women in " + state.name + " have no personal doctor or other health care provider." + "</p>" + "<p>" + state.health_insurance + "% of women aged 19-63 did not have health insurance coverage.</p>")
+                    
+
+                    $("#container1").html(educationContainer1)
+                    $("#container2").html(educationContainer2) 
+                    $("#container3").html(educationContainer3)
                 }
 
                 $.getJSON('/states/' + svgState.key)
