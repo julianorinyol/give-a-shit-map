a4c25b5e37048d9418bb1e0856e6d7859e33aac5
JulianOrinyol <julianorinyol@gmail.com>
2015-04-13 11:24:47 -0700

    added procfile, updated some config files to prepare for heroku and postgres deploy

--- Gemfile
+++ Gemfile
@@ -7,7 +7,6 @@ gem 'activesupport'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'sinatra-activerecord'
-gem 'pg'
 gem 'puma'
 gem 'tux'
 gem 'jbuilder'
@@ -16,4 +15,8 @@ group :development, :test do
   gem 'pry'
   gem 'shotgun'
   gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
 end
--- config/database.rb
+++ config/database.rb
@@ -9,10 +9,14 @@ configure do
 
   puts "connecting to database..."
 
+if development?
   set :database, {
     adapter: "sqlite3",
     database: "db/db.sqlite3"
   }
+else
+  set :database, ENV['DATABASE_URL']
+end
   # set :database, {
   #   adapter: "postgres",
   #   database: DATABASE
